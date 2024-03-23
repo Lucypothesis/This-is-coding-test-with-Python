eb828ba5abdb778626c75f190700a630221b7355
Lucy <164493549+Lucypothesis@users.noreply.github.com>
2024-03-23 11:28:08 +0900

    Add files via upload
    p. 115 왕실의 나이트
    2차원 평면벡터 방법 몰랐을 때는 빡쳤는데
    방법 쓰니 잘 풀림

---
+++ 4-3.py
@@ -0,0 +1,15 @@
+input = input()
+x = int(input[1])
+list = 'abcdefgh'
+y = list.index(input[0]) + 1
+
+dx = [2, 2, -2, -2, 1, 1, -1, -1]
+dy = [1, -1, 1, -1, 2, -2, 2, -2]
+count = 0
+
+for i in range(8):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    if 1 <= nx <= 8 and 1 <= ny <= 8:
+        count += 1
+print(count)
